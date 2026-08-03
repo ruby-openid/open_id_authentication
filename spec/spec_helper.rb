1c296397c343aea6463c1b3431b635f17e9a516a
Peter H. Boling <peter.boling@gmail.com>
2026-08-03 10:03:33 -0600

    🎨 Template bootstrap by kettle-jem v7.1.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 # RSpec Configs
-require "debug"
+require "debug" if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new("2.7")
 require "config/rspec/rspec_block_is_expected"
 require "config/rspec/rspec_core"
 require "config/rspec/version_gem"
